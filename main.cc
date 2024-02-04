354eddf78e365abd0e0970ed73abd04725f0cb51
Ryan Vickramasinghe <ryanvickr@gmail.com>
2024-02-03 20:15:42 -0800

    Import wasmtime library and create skeleton management class.

--- main.cc
+++ main.cc
@@ -1,8 +1,14 @@
 // Author: Ryan Vickramasinghe
 
 #include <iostream>
+#include <memory>
+
+#include "manager/wasm-manager.h"
+
+using namespace wasm_experiment;
 
 int main() {
+    const auto wasm_manager = std::make_unique<manager::WasmManager>();
     // TODO: Run a binary.
     std::cout << "Running a WASM function." << std::endl;
     return 0;
--- manager/wasm-manager.h
+++ manager/wasm-manager.h
@@ -1,1 +1,22 @@
 // Author: Ryan Vickramasinghe
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+
+#include "wasmtime.hh"
+
+namespace wasm_experiment {
+namespace manager {
+
+class WasmManager {
+ public:
+    explicit WasmManager() {};
+
+ private:
+    std::unique_ptr<int> engine_;
+};
+
+}  // manager
+}  // wasm_experiment
