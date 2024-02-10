2ff4d84572635f5ce4c27ca90ee2bca680547eb6
Ryan Vickramasinghe <ryanvickr@gmail.com>
2024-02-10 14:00:43 -0800

    Added a hello world wat file which can be loaded into the WasmManager.

--- wasm-binary/hello-world.cc
+++
@@ -1,9 +0,0 @@
-// Author: Ryan Vickramasinghe
-// This is a just a very simple hello world function that serves
-// to prove that we can use a language agnostic WASM.
-
-#include <iostream>
-
-void GenericFunction() {
-    std::cout << "Hello world!" << std::endl;
-}
